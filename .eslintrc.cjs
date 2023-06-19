d26413ad029c73245c180cd33d5a3664795ac69e
Martín Gesualdo <mgesudev@gmail.com>
2023-06-19 20:22:18 -0300

    Cambios

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,34 @@
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const path = require("path")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path")
 
-// /** @type {import("eslint").Linter.Config} */
-// const config = {
-//   overrides: [
-//     {
-//       extends: [
-//         "plugin:@typescript-eslint/recommended-requiring-type-checking",
-//       ],
-//       files: ["*", "*.tsx"],
-//       parserOptions: {
-//         project: path.join(__dirname, "tsconfig.json"),
-//       },
-//     },
-//   ],
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     project: path.join(__dirname, "tsconfig.json"),
-//   },
-//   plugins: ["@typescript-eslint"],
-//   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-//   rules: {
-//     "no-unsafe-assignment": "off",
-//     "consistent-type-imports": "off",
-//     "no-unused-vars": "off",
-//     "no-unsafe-return": "off",
-//     "no-unsafe-member-access": "off",
-//     "no-unsafe-call": "off",
-//   },
-// }
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*", "*.tsx"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "no-unsafe-assignment": "off",
+    "consistent-type-imports": "off",
+    "no-unused-vars": "off",
+    "no-unsafe-return": "off",
+    "no-unsafe-member-access": "off",
+    "no-unsafe-call": "off",
+  },
+}
 
-// module.exports = config
+module.exports = config
