cb536d55443e91dd0fb78c9b5da698259f6ca5de
Martín Gesualdo <mgesudev@gmail.com>
2023-07-04 09:21:06 -0300

    Cambios

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,32 +30,32 @@ const config = {
   //   ],
   //   "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   // },
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*", "*.tsx"],
-      parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "no-unsafe-assignment": "off",
-    "consistent-type-imports": "off",
-    "no-unused-vars": "off",
-    "no-unsafe-return": "off",
-    "no-unsafe-member-access": "off",
-    "no-unsafe-call": "off",
-  },
+  // overrides: [
+  //   {
+  //     extends: [
+  //       "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  //     ],
+  //     files: ["*", "*.tsx"],
+  //     parserOptions: {
+  //       project: path.join(__dirname, "tsconfig.json"),
+  //     },
+  //   },
+  // ],
+  // parser: "@typescript-eslint/parser",
+  // parserOptions: {
+  //   project: path.join(__dirname, "tsconfig.json"),
+  //   sourceType: "module",
+  // },
+  // plugins: ["@typescript-eslint"],
+  // extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  // rules: {
+  //   "no-unsafe-assignment": "off",
+  //   "consistent-type-imports": "off",
+  //   "no-unused-vars": "off",
+  //   "no-unsafe-return": "off",
+  //   "no-unsafe-member-access": "off",
+  //   "no-unsafe-call": "off",
+  // },
 }
 
 module.exports = config
