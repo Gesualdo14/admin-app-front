33204b359f3809ea876e1da9f03f9b8a7d1f89c7
Martín Gesualdo <mgesudev@gmail.com>
2023-07-16 09:02:36 -0300

    Cambios

--- helpers/getProductDiscount.ts
+++ helpers/getProductDiscount.ts
@@ -4,7 +4,7 @@ import { ProductForState } from "schemas/SaleSchema"
 const getProductDiscount = (product: ProductFromDB | ProductForState) => {
   const discount = (product.discount || 0) * 100
   const isDecimal = discount % 1 !== 0
-  const formattedDiscount = "%" + discount.toFixed(isDecimal ? 2 : 0) + " OFF"
+  const formattedDiscount = discount.toFixed(isDecimal ? 2 : 0) + "% OFF"
   return { discount, formattedDiscount }
 }
 
